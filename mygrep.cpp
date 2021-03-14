8832a518ea0d89fdba0b8d84e9e832a571e609f4
Vilppu Tupeli <vilppu.tupeli@tuni.fi>
2021-03-14 20:11:38 +0200

    added flags i and r

--- mygrep.cpp
+++ mygrep.cpp
@@ -1,4 +1,5 @@
 // TODO: proper user guide here
+// TODO: rename subStr to sub_string
 /* 
 Running the program:
 ./mygrep -flags searchword filename.txt 
@@ -15,33 +16,40 @@ using namespace std;
 const char* FLAG_OPTIONS_CHAR = "-o";
 const char* FLAG_OCCURRENCES_CHAR = "o";
 const char* FLAG_LINE_NUMBERS_CHAR = "l";
+const char* FLAG_REVERSE_SEARCH_CHAR = "r";
+const char* FLAG_IGNORE_CASE_CHAR = "i";
+
+// Prototypes
+void search_from_file(string sub_string, string filename, bool show_line_numbers, bool show_occurrences, bool reverse_search, bool ignore_case);
+string string_to_lower(string str);
 
 
 
-/*
-void output(int found_index, string subStr, string bigStr)
+// converts a string to all lowercase.
+void to_lower_case(string& str)
 {
-	// Printing output
-	if (found_index != string::npos)
-	{
-		cout << "\"" <<subStr << "\" found in \"" << bigStr << "\" in position " << found_index << endl;
-	}
-	else
-	{
-		cout <<"\"" << subStr << "\" NOT found in \"" << bigStr << "\"" << endl;
-	}
+   for(int i = 0; i < str.length(); i++)
+   {
+      str[i] = tolower(str[i]);
+   }
 }
-*/
 
-void search_from_file(string subStr, string filename, bool show_line_numbers, bool show_occurrences)
+
+void search_from_file(string sub_string, string filename, bool show_line_numbers, bool show_occurrences, bool reverse_search, bool ignore_case)
 {
-	// Read lines from file and print them to console.
 	fstream searchfile;
 	searchfile.open(filename, ios::in);
 	
-	// declare variables
-	int line_number = 0, occurrences = 0;
-	string file_line;
+	// declaring variables
+	int line_number = 0, occurrences = 0, not_occurrences = 0;
+	string file_line, file_line_temp, sub_string_temp = sub_string;
+	bool sub_string_found = false;
+
+	
+	if (ignore_case)
+	{
+		to_lower_case(sub_string_temp);
+	}
 
 	if (!searchfile)
 	{
@@ -54,34 +62,86 @@ void search_from_file(string subStr, string filename, bool show_line_numbers, bo
 		while (!searchfile.eof())
 		{
 			line_number++;
-			getline(searchfile, file_line); 
+			sub_string_found = false;
+			getline(searchfile, file_line);
 
-			if (file_line.find(subStr) != string::npos)
+			// if ignore_case is true, convert file_line_temp to lowercase. 
+			if (ignore_case)
+			{
+				file_line_temp = file_line;
+				to_lower_case(file_line_temp);
+
+				//testing
+				//cout << "sub_string: " << sub_string << "sub_string_temp: " << sub_string_temp << endl;
+				//cout << "file_line " << file_line << "file_line_temp: " << file_line_temp << endl;
+			} 
+
+			if (file_line_temp.find(sub_string_temp) != string::npos)
+			{
+				sub_string_found = true;
+			}
+
+
+			// 			if ((file_line_temp.find(sub_string_temp) != string::npos) and !reverse_search)
+			if (sub_string_found and !reverse_search)
 			{
 				occurrences++;
 				
 				// if show_line_numbers is true also print line line numbers,
 				// if not print only line content.
-				if(show_line_numbers)
+				//TODO: might be able to make this neater
+				
+				if (show_line_numbers)
+				{
+					cout << line_number << ":";
+				}
+				
+				cout << file_line << endl;
+			}
+			
+			else if (!sub_string_found and reverse_search)
+			{
+				not_occurrences++;
+				
+				if	(show_line_numbers)
+				{
+					cout << line_number << ":";
+				}
+				
+				cout << file_line << endl;
+				
+				/*
+				if	(show_line_numbers)
 				{
 					cout << line_number << ":" << file_line << endl;
 				}
-				
-				else {cout << file_line << endl;}
+				else
+				{
+					cout << file_line << endl;
+				}
+				*/
 			}
+
+			sub_string_found = false;
 		}
 	}
 
-	if(show_occurrences)
+	// Printing to be done if show occurrences is
+	if (show_occurrences and !reverse_search)
 	{
-		cout << "\nOccurrences of lines containing \"" << subStr << "\": " << occurrences << endl;
+		cout << "\nOccurrences of lines containing \"" << sub_string << "\": " << occurrences << endl;
+	}
+
+	else if (show_occurrences and reverse_search)
+	{
+		cout << "\nOccurrences of lines NOT containing \"" << sub_string << "\": " << not_occurrences << endl;
 	}
 }
 
 
 int main(int argc, char* argv[])
 {
-	// TODO: consider moving these to their approprriate satements.
+	// TODO: consider moving these to their appropriate satements.
 	int found_index;
 	string bigStr, subStr, filename;
 
@@ -89,7 +149,11 @@ int main(int argc, char* argv[])
 	//bool flag_options = false; // TODO: flag_options may be unneccessary! 
 	bool flag_show_occurrences = false;
 	bool flag_show_line_numbers = false;
+	bool flag_reverse_search = false;
+	bool flag_ignore_case = false;
 	
+	// TODO: consider dividing these into functions, should bee quite smooth, also make prototypes
+
 	// ask string and substring.
 	// Done only when arguments are given.
 	if (argc == 1)
@@ -123,7 +187,7 @@ int main(int argc, char* argv[])
 	{
 		subStr = argv[1];
 		filename = argv[2];
-		search_from_file(subStr, filename,flag_show_line_numbers, flag_show_occurrences);
+		search_from_file(subStr, filename, flag_show_line_numbers, flag_show_occurrences, flag_reverse_search, flag_ignore_case);
 	}
 
 
@@ -136,11 +200,6 @@ int main(int argc, char* argv[])
 	{
 		subStr = argv[2];
 		filename = argv[3];
-
-
-
-		// check what flags are given.
-		// couts are for testing
 		
 		// Check that "-o" is first in argv[1].
 		if (strstr(argv[1], FLAG_OPTIONS_CHAR) == argv[1])
@@ -149,21 +208,28 @@ int main(int argc, char* argv[])
 			// Remove first two elements ("-" and "o" ) from argv[1] 
 			argv[1] = &argv[1][2];
 			
-			//flag_options = true; // this is may not be necessary
-
-
+			// look for flag characters in argv[1] and set corresponding flags to true if found.
 			if (strstr(argv[1], FLAG_OCCURRENCES_CHAR))
 			{
 				flag_show_occurrences = true;
 			}
-
 
 			if (strstr(argv[1], FLAG_LINE_NUMBERS_CHAR))
 			{
 				flag_show_line_numbers = true;
 			}
 
-			search_from_file(subStr, filename,flag_show_line_numbers, flag_show_occurrences);
+			if (strstr(argv[1], FLAG_REVERSE_SEARCH_CHAR))
+			{
+				flag_reverse_search = true;
+			}
+
+			if (strstr(argv[1], FLAG_IGNORE_CASE_CHAR))
+			{
+				flag_ignore_case = true;
+			}
+
+			search_from_file(subStr, filename,flag_show_line_numbers, flag_show_occurrences, flag_reverse_search, flag_ignore_case);
 		}
 
 		// incorrect flags, give user and error message.
