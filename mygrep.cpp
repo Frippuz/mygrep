5a811ea5ff31ccc35bfa3f520fb64dfeca3c2031
Vilppu Tupeli <vilppu.tupeli@tuni.fi>
2021-03-14 22:07:06 +0200

    Fixed bug and made code prettier.

--- mygrep.cpp
+++ mygrep.cpp
@@ -1,5 +1,3 @@
-// TODO: proper user guide here
-// TODO: rename subStr to sub_string
 /* 
 Running the program:
 ./mygrep -flags searchword filename.txt 
@@ -12,7 +10,6 @@ flags start with -o
 using namespace std;
 
 // Declaring flags as constants 
-// TODO: Do these need to be global?
 const char* FLAG_OPTIONS_CHAR = "-o";
 const char* FLAG_OCCURRENCES_CHAR = "o";
 const char* FLAG_LINE_NUMBERS_CHAR = "l";
@@ -21,22 +18,22 @@ const char* FLAG_IGNORE_CASE_CHAR = "i";
 
 // Prototypes
 void search_from_file(string sub_string, string filename, bool show_line_numbers, bool show_occurrences, bool reverse_search, bool ignore_case);
-string string_to_lower(string str);
+void to_lower_case(string& str);
 
 
 
-// converts a string to all lowercase.
 void to_lower_case(string& str)
 {
-   for(int i = 0; i < str.length(); i++)
-   {
-      str[i] = tolower(str[i]);
-   }
+	// Convert a string to all lower case
+	for(int i = 0; i < str.length(); i++)
+	{
+		str[i] = tolower(str[i]);
+	}
 }
-
 
 void search_from_file(string sub_string, string filename, bool show_line_numbers, bool show_occurrences, bool reverse_search, bool ignore_case)
 {
+	// Function that handles reading file and searching from file.
 	fstream searchfile;
 	searchfile.open(filename, ios::in);
 	
@@ -45,35 +42,34 @@ void search_from_file(string sub_string, string filename, bool show_line_numbers
 	string file_line, file_line_temp, sub_string_temp = sub_string;
 	bool sub_string_found = false;
 
-	
+	// make sub_string_temp lowercase if ignore_case is true.
 	if (ignore_case)
 	{
 		to_lower_case(sub_string_temp);
 	}
 
+
+	// Check that file could be read correctly.
 	if (!searchfile)
 	{
-		// TODO: check output 
-		cout << "error: Could not open file." << endl;
+		cout << "An exception occurred." << endl;
 	}
 
-	else
+
+	// searching for occurrences and countign them.
+	else 
 	{
 		while (!searchfile.eof())
 		{
 			line_number++;
-			sub_string_found = false;
 			getline(searchfile, file_line);
-
+			file_line_temp = file_line;
+			
 			// if ignore_case is true, convert file_line_temp to lowercase. 
 			if (ignore_case)
 			{
-				file_line_temp = file_line;
+				
 				to_lower_case(file_line_temp);
-
-				//testing
-				//cout << "sub_string: " << sub_string << "sub_string_temp: " << sub_string_temp << endl;
-				//cout << "file_line " << file_line << "file_line_temp: " << file_line_temp << endl;
 			} 
 
 			if (file_line_temp.find(sub_string_temp) != string::npos)
@@ -82,7 +78,6 @@ void search_from_file(string sub_string, string filename, bool show_line_numbers
 			}
 
 
-			// 			if ((file_line_temp.find(sub_string_temp) != string::npos) and !reverse_search)
 			if (sub_string_found and !reverse_search)
 			{
 				occurrences++;
@@ -109,24 +104,15 @@ void search_from_file(string sub_string, string filename, bool show_line_numbers
 				}
 				
 				cout << file_line << endl;
-				
-				/*
-				if	(show_line_numbers)
-				{
-					cout << line_number << ":" << file_line << endl;
-				}
-				else
-				{
-					cout << file_line << endl;
-				}
-				*/
 			}
 
 			sub_string_found = false;
 		}
 	}
 
-	// Printing to be done if show occurrences is
+	searchfile.close();
+
+	// Printed if show_occurrences is true
 	if (show_occurrences and !reverse_search)
 	{
 		cout << "\nOccurrences of lines containing \"" << sub_string << "\": " << occurrences << endl;
@@ -141,18 +127,15 @@ void search_from_file(string sub_string, string filename, bool show_line_numbers
 
 int main(int argc, char* argv[])
 {
-	// TODO: consider moving these to their appropriate satements.
 	int found_index;
 	string bigStr, subStr, filename;
 
 	// Declaring option flags as boolean values.
-	//bool flag_options = false; // TODO: flag_options may be unneccessary! 
 	bool flag_show_occurrences = false;
 	bool flag_show_line_numbers = false;
 	bool flag_reverse_search = false;
 	bool flag_ignore_case = false;
 	
-	// TODO: consider dividing these into functions, should bee quite smooth, also make prototypes
 
 	// ask string and substring.
 	// Done only when arguments are given.
@@ -190,10 +173,6 @@ int main(int argc, char* argv[])
 		search_from_file(subStr, filename, flag_show_line_numbers, flag_show_occurrences, flag_reverse_search, flag_ignore_case);
 	}
 
-
-
-
-	// TODO: make function call when flags are given.
 
 	// When flags, searchword and filename are given.
 	else if (argc == 4 and argv[2] != "")
@@ -240,34 +219,10 @@ int main(int argc, char* argv[])
 	}
 
 
-	// If no searchword is given. 
 	else 
 	{
-		
-		// TODO: give user proper error message!
-		
-		cout << "error" << endl;
-		
-		cout << argc << " " << argv << endl;
-
-		for(int i = 0 ; i < sizeof(*argv); i++)
-		{
-			cout << "argument " << i << ": " << argv[i] << endl;
-		}
+		cout << "An exception occurred. No searchword given." << endl;
 	}
 
 	return EXIT_SUCCESS;
-	
-
-// TODO: delete these
-# if 0
-	// These may be needed later
-	
-	// Converting strings to C-style const char*
-	const char* bigStr_C = bigStr.c_str();
-	const char* subStr_C = subStr.c_str();
-
-	const char* result = strstr(bigStr_C, subStr_C);
-	cout << result << endl;
-#endif
 }
